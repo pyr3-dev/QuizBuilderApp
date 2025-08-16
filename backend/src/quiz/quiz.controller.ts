68bbce202533379060a0ed01257f8cbd91092ba2
pyr3-dev <61533949+pyr3-dev@users.noreply.github.com>
2025-08-17 02:47:17 +0800

    Add QuizController with REST API endpoints

---
+++ backend/src/quiz/quiz.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  ValidationPipe,
+  UsePipes,
+} from '@nestjs/common';
+import { QuizService } from './quiz.service';
+import { CreateQuizDto } from './dto/create-quiz.dto';
+import { UpdateQuizDto } from './dto/update-quiz.dto';
+
+@Controller('quizzes')
+export class QuizController {
+  constructor(private readonly quizService: QuizService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe({ transform: true, whitelist: true }))
+  create(@Body() createQuizDto: CreateQuizDto) {
+    return this.quizService.create(createQuizDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.quizService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.quizService.findOne(id);
+  }
+
+  @Patch(':id')
+  @UsePipes(new ValidationPipe({ transform: true, whitelist: true }))
+  update(@Param('id') id: string, @Body() updateQuizDto: UpdateQuizDto) {
+    return this.quizService.update(id, updateQuizDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.quizService.remove(id);
+  }
+}
